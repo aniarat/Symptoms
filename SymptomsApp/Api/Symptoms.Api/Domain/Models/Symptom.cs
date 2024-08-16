7069481c6a6bd5abb8098860664a1869d8606498
aniarat <anka.ratowska@gmail.com>
2024-08-16 15:19:00 +0200

    added db, models

---
+++ SymptomsApp/Api/Symptoms.Api/Domain/Models/Symptom.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Symptoms.Api.Domain.Models
+{
+    public class Symptom
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("created_at"), BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonElement("pain_type"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        [Required(ErrorMessage = "PainType must be specified.")]
+        public PainTypes PainType { get; set; }
+
+        [BsonElement("severity_scale"), BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        [Range(1, 10, ErrorMessage = "Severity scale must be a number between 1 and 10.")]
+        public int SeverityScale { get; set; }
+
+        [BsonElement("duration_hours"), BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int? SymptomDurationHours { get; set; }
+
+        [BsonElement("occurence_counter"), BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int OccurrenceCounter { get; set; }
+
+
+        public enum PainTypes
+        {
+            Headache,
+            Stomachache,
+            BackPain,
+            AbdominalPain,
+            ChestPain,
+            MusclePain
+        }
+    }
+}
---
+++ SymptomsApp/Api/Symptoms.Api/Domain/Models/SymptomHistory.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Symptoms.Api.Domain.Models
+{
+    public class SymptomHistory
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        public string SymptomId { get; set; }
+
+        public string FieldName { get; set; }
+
+        public string OldValue { get; set; }
+
+        public string NewValue { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
